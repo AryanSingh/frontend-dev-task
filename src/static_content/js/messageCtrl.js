571468debbfe0ba23d18a3de0bbdd01621c006c5
AryanSingh <aryan@iitgn.ac.in>
2016-04-28 19:01:08 +0530

    delete button added on message page

--- src/static_content/js/messageCtrl.js
+++ src/static_content/js/messageCtrl.js
@@ -1,12 +1,20 @@
 (function(){
   'use strict';
-  angular.module('devApp').controller('MessageCtrl', ['messageApi',MessageCtrl]);
+  angular.module('devApp').controller('MessageCtrl', ['messageApi','Flash','$location',MessageCtrl]);
 
-  function MessageCtrl(messageApi){
+  function MessageCtrl(messageApi,Flash,$location){
     var vm = this;
     messageApi.getMessages().then(function(data){
       vm.messages = data;
-      
     })
+    vm.messageDelete =function(id){
+      messageApi.deleteMessage(id);
+      $location.path("/messages");
+      var message = 'You successfully deleted the message.';
+      var id = Flash.create('success', message, 2000, {class: 'custom-class', id: 'custom-id'}, true);
+      messageApi.getMessages().then(function(data){
+        vm.messages = data;
+      })
+    }
   };
 })();
